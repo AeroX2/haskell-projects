eb95d46114a9696aea7b61dc9b5e809454e18ba3
AeroX2 <james@ridey.email>
2017-06-18 01:50:22 +1000

    Bug fixes for the calcs

--- Infix.hs
+++ Infix.hs
@@ -3,7 +3,7 @@ import Data.List.Split
 import Text.Read
 import qualified Data.Map as Map
 
---import Debug.Trace
+-- import Debug.Trace
 import RPN
 
 main :: IO ()
@@ -22,17 +22,21 @@ operators = Map.fromList [
 
 infix' :: String -> [String] -> [String] -> String
 infix' input istack ostack -- | trace ("infix " ++ show (reverse ostack) ++ " " ++ show istack ++ " " ++ show input) False = undefined
-        | null input         = show $ rpn (reverse (unwords (istack++ostack))) []  -- rpn (unwords $ reverse $ ostack++istack) []
+        | null input         = show $ rpn final []
         | is_number token    = infix' rest istack (token:ostack)
         | in_operators token = infix' rest (token:pop_istack (\z -> precedence z >= precedence token)) (popr_istack (\z -> precedence z >= precedence token) ++ ostack)
         | token == "("       = infix' rest (token:istack) ostack
         | token == ")"       = infix' rest (tail (pop_istack (/="("))) (popr_istack (/="(") ++ ostack)
         | otherwise          = infix' rest istack ostack
         where
-            (token:xs) = split (dropInitBlank . dropFinalBlank . dropInnerBlanks . oneOf $ "()"++concat (Map.keys operators)) input
+            (token:xs) = split (dropInitBlank . dropFinalBlank . dropInnerBlanks . oneOf $ " ()"++concat (Map.keys operators)) input
             rest = concat xs
+
             is_number x = isJust (readMaybe x :: Maybe Int)
             in_operators x = Map.member x operators 
             precedence x = if in_operators x then operators Map.! x else (-1)
+
             pop_istack f = dropWhile f istack
             popr_istack f = takeWhile f istack
+
+            final = unwords $ (reverse ostack) ++ (istack)
--- RPN.hs
+++ RPN.hs
@@ -1,17 +1,19 @@
 module RPN (rpn) where
 
+-- import Debug.Trace
+
 main :: IO ()
 main = do
         input <- getLine
         print $ rpn input []
 
 rpn :: String -> [Float] -> Float
-rpn input stack 
+rpn input stack -- | trace ("rpn " ++ show (stack) ++ " " ++ show input) False = undefined
             | null input = head stack
             | hi == "+" = rpn (unwords ti) (x+y:st)
-            | hi == "-" = rpn (unwords ti) (x-y:st)
+            | hi == "-" = rpn (unwords ti) (y-x:st)
             | hi == "*" = rpn (unwords ti) (x*y:st)
-            | hi == "/" = rpn (unwords ti) (x/y:st)
+            | hi == "/" = rpn (unwords ti) (y/x:st)
             | otherwise = rpn (unwords ti) (number:stack)
             where
                 (x:y:st) = stack
